f9cd97156359363b0d309bbad0e097cbf2452695
emilio <emilio@aguilar.es>
2023-01-31 19:58:22 +0000

    20230322

---
+++ src/main/java/inclui/hola_mundo/Hola_mundo.java
@@ -0,0 +1,12 @@
+package inclui.hola_mundo;
+
+/**
+ * Aplicación básica en Java con Maven
+ * @author emilio
+ */
+public class Hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
